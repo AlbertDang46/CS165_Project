b0a81b0b4bfb2644f865dd9ecbdc92081b250392
Payas <payas.rajan@email.ucr.edu>
2020-05-12 15:55:35 -0700

    Added client and server code

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,37 +2,42 @@
 
 # Always start from the main directory
 PROJECT_DIR=$(pwd)
+PROJECT_BUILD_DIR=$PROJECT_DIR/build
 echo "Starting Setup... $PROJECT_DIR"
 
 NUM_PARALLEL_BUILDS=12
 
-# 1. Extract the archived third party libraries
 EXTERN_DIR=$PROJECT_DIR/extern
-LIBRESSL_DIR=$EXTERN_DIR/libressl
-LIBRESSL_INSTALL_DIR=$EXTERN_DIR/libressl_install
 CMAKE_DIR=$EXTERN_DIR/cmake
+CMAKE_BIN_DIR=$CMAKE_DIR/bin
+LIBRESSL_DIR=$EXTERN_DIR/libressl
+LIBRESSL_BUILD_DIR=$LIBRESSL_DIR/build
+LIBRESSL_INSTALL_DIR=$EXTERN_DIR/libressl_install
 
+# 1. Extract the archived third party libraries
 mkdir $LIBRESSL_DIR
 mkdir $CMAKE_DIR
 tar -xvzf $EXTERN_DIR/libressl-3.1.1.tar.gz -C $LIBRESSL_DIR --strip-components=1
 tar -xvzf $EXTERN_DIR/cmake-3.17.2-Linux-x86_64.tar.gz -C $CMAKE_DIR --strip-components=1
 
 # 2. Build libressl
-LIBRESSL_BUILD_DIR=$LIBRESSL_DIR/build
-CMAKE_BIN_DIR=$CMAKE_DIR/bin
-
 mkdir $LIBRESSL_BUILD_DIR
 mkdir $LIBRESSL_INSTALL_DIR
 cd $LIBRESSL_BUILD_DIR
 $CMAKE_BIN_DIR/cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH="$LIBRESSL_INSTALL_DIR" -DBUILD_SHARED_LIBS=ON $LIBRESSL_DIR
 make -j$NUM_PARALLEL_BUILDS
-make install
-
 echo "LibreSSL built!"
 
-# 3. Build code
-BUILD_DIR=$PROJECT_DIR/build
-mkdir $BUILD_DIR && cd $BUILD_DIR
+make install
+echo "LibreSSL installed to: $LIBRESSL_INSTALL_DIR"
 
+echo "Setting up environment variables"
+LD_LIBRARY_PATH="$LIBRESSL_INSTALL_DIR/lib/":$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH
+
+# 3. Build code
+mkdir $PROJECT_BUILD_DIR && cd $PROJECT_BUILD_DIR
 $CMAKE_BIN_DIR/cmake -DCMAKE_BUILD_TYPE=Release -DLIBRESSL_ROOT_DIR="$EXTERN_DIR/libressl_install" $PROJECT_DIR
 make -j$NUM_PARALLEL_BUILDS
+
+
--- src/hello_world.cpp
+++
@@ -1,8 +0,0 @@
-#include <tls.h>
-#include <iostream>
-
-int main() {
-	tls_init();
-	std::cout << "Hello world!" << std::endl;
-	return 0;
-}
